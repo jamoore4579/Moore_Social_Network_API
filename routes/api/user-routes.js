ad8ab835c032b73a78e5d915d57aeb405e4a1d79
Irish Moore <jasonamoore@frontier.com>
2022-03-30 15:49:54 -0400

    built user route

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,3 +1,38 @@
 const router = require('express').Router();
 
+const {
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    getAllusers,
+    removeFriend
+} = require('../../controllers/user-controller');
+
+//api routes for GET all and POST at /api/users
+router
+    .route('/')
+    .get()
+    .post()
+
+// /api/users/:id
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser)
+
+// /api/users/:userId/friends/:friendId
+router
+    .route('/:id/friends/:friendId')
+    .post(addFriend)
+    .delete(removeFriend)
+
+// /api/users
+router
+    .route('/')
+    .get(getAllusers)
+    .post(createUser)
+
 module.exports = router;
