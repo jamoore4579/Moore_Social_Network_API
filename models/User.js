933492b3030f1b36e19462472b455d97a14a819a
Irish Moore <jasonamoore@frontier.com>
2022-03-30 15:19:52 -0400

    user model coded

--- models/User.js
+++ models/User.js
@@ -1,3 +1,46 @@
 const { Schema, model } = require('mongoose');
 
+// user schema
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/, 'Please submit a valid email address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+const User = model('User', UserSchema);
+
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
 module.exports = User;
