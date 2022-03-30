62e6446eb8af5d1678e4cad89919fe130bd8ec73
Irish Moore <jasonamoore@frontier.com>
2022-03-30 16:29:34 -0400

    built out the thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,3 +1,43 @@
 const { schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+const reactionSchema = require('./Reaction');
+
+// thought schema
+const thoughtSchema = new schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        createAt : {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        // bringing in reactions
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+// total count of friends
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+// create the User model using UserSchema
+const Thought = model('Thought', thoughtSchema)
+
 
 module.exports = { Thought };
